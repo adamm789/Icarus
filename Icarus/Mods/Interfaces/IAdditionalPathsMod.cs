01adf2befd0f2545a026fb9b846ce259672efc10
adamm789 <musician903@gmail.com>
2023-01-08 12:09:34 -0600

    Create IAdditionalPathsMod.cs
    to allow assigning to all material variants

---
+++ Icarus/Mods/Interfaces/IAdditionalPathsMod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icarus.Mods.Interfaces
+{
+    public interface IAdditionalPathsMod : IMod
+    {
+        bool AssignToAllPaths { get; }
+        Dictionary<string, string> AllPathsDictionary { get; }
+    }
+}
